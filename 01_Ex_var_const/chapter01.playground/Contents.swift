43e9d12cbe6a4f60b24bc70dbc7071d6f41af84e
NguyenT01 <transinguyen2001@gmail.com>
2022-06-05 15:08:32 +0700

    complete chapter01

---
+++ 01_Ex_var_const/chapter01.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// This is single line comment
+
+/* This is multi line
+    comment
+ */
+
+print("Hello world!!!")
+
+2+7
+15-6
+8*4
+40/5
+
+// decimal numbers
+22/7.0
+22.0/7
+
+16>>2
+
+// DATA TYPES
+//CONSTANT
+let number: Int = 4
+
+let pi: Double = 3.141519
+
+//VARIABLES
+var x1: Int = 42
+x1 = 33
+x1 = 1_000_000
+
+x1 += 1
+x1 -= 1
+
