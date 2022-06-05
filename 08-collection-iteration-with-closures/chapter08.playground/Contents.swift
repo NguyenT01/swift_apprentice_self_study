6b288ff70c68c323c03ba8ad5fbeeb4d771125d7
NguyenT01 <transinguyen2001@gmail.com>
2022-06-05 21:18:40 +0700

    chapter08 complete

--- 08-collection-iteration-with-closures/chapter08.playground/Contents.swift
+++ 08-collection-iteration-with-closures/chapter08.playground/Contents.swift
@@ -1,2 +1,39 @@
 import UIKit
 
+// CLOSURE
+//var multiplyClosure: (Int, Int) -> Int
+var multiplyClosure = {
+    (a: Int, b: Int)-> Int in
+    return a*b
+}
+
+let result = multiplyClosure(4,2)
+
+//SHORTHAND SYNTAX
+multiplyClosure = {
+    (a,b) in
+    a * b
+}
+
+multiplyClosure = {
+    $0 * $1
+}
+
+//------
+func operateOnNumbers(_ a: Int, _ b: Int, operation: (Int, Int)-> Int) -> Int{
+    let result = operation(a,b)
+    print(result)
+    return result
+}
+
+let addClosure = {
+    (a: Int, b: Int) in
+    a + b
+}
+operateOnNumbers(4, 2, operation: addClosure)
+
+//-------
+let values = [1,2,3,4,5,6]
+values.forEach{
+    print("\($0) : \($0*$0)")
+}
